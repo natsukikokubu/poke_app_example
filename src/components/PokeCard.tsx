d5654a1b3046ce12166bd016e2af8a1141683511
Yuto Tachibana <tachibanayu24@gmail.com>
2024-02-17 18:46:48 +0900

    Tweak

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -7,17 +7,17 @@ type Props = {
 export const PokeCard = ({ name }: Props) => {
   const [imgUrl, setImgUrl] = useState<string | null>();
 
-  const handleClickShowDetails = async () => {
+  const handleClickShowImage = async () => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data: PokemonDetailAPI = await response.json();
-    setImgUrl(data.sprites.front_default);
+    setImgUrl(data.sprites.back_default);
   };
 
   return (
     <div className="card-wrapper">
       <h2>{name}</h2>
       {imgUrl && <img alt={name} src={imgUrl} />}
-      <button onClick={handleClickShowDetails}>Show Image</button>
+      <button onClick={handleClickShowImage}>Show Image</button>
     </div>
   );
 };
